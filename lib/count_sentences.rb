c9f09a343bca30c163c7f30b57b41e890c976355
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-27 01:08:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[!.?]/) 
+    short_array = arr.select do |x|
+      x != ""
+    end
+    short_array.length 
   end
 end
+
+
+
